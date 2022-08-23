bd51149ad70ba1b90be025a6a87b70ddcd5fa10f
mouheb <mbenchiekh@ats-digital.com>
2022-08-23 15:59:23 +0100

    Revert "Revert "auth with google fastify/auth""
    This reverts commit 4fd567baf0f507cb8a17ef630fda2a3d02407cd2.

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+import cors from '@fastify/cors';
+import Fastify from 'fastify';
+import authRoute from './plugins/auth';
+const fastify = Fastify({ logger: { transport: {
+            target: 'pino-pretty',
+            options: {
+                translateTime: 'HH:MM:ss Z',
+                ignore: 'pid,hostname'
+            }
+        }
+    } });
+await fastify.register(cors, {
+    "origin": "*",
+    // put your options here
+});
+fastify.get('/ping', async (request, reply) => {
+    return 'pong\n';
+});
+fastify.register(authRoute);
+fastify.listen({ port: 3000 }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
